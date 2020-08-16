a38bdf38b2e89695b27a740ce0e37623347b426c
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-08-16 14:32:32 +0200

    merging from stash

--- src/components/Show/ShowPoster/ShowPoster.js
+++ src/components/Show/ShowPoster/ShowPoster.js
@@ -1,12 +1,18 @@
 import React, { Fragment } from 'react';
 import '../Show.css';
+// import not_available from './not_available.png';
 
 function ShowPoster(props) {
+    let poster = props.poster;
+    if(props.poster.length === 0){
+        poster = process.env.PUBLIC_URL + '/not_available.png';
+    }
+
     return (
         <Fragment>
             <img className="img db ba b--black-10"
-                    alt="poster"
-                    src={props.poster} />
+                    alt='poster'
+                    src={poster} />
         </Fragment>
     );
 }
